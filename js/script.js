2ba50e93be4cfa8c094476df2b6ea7477fd2255e
trueSlav <2001idercach@gmail.com>
2023-09-08 01:53:06 +0300

    A Открывающий скрипт

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-	console.log('hi');
-
 	
+	const wrapper = document.querySelector('.wrapper'),
+				button = document.querySelectorAll('.hero-section__round');
+	
+	const closeBtn = () => {
+		button.forEach(item => {
+			if(item.classList.contains('active')){
+				item.classList.remove('active');
+			}
+		})
+	}
+	
+	const openBtn = (i) => {
+		button[i].classList.add('active');
+	}
+
+	wrapper.addEventListener('click', e => {
+		console.log(e.target);
+
+		if(e.target && e.target.classList.contains('hero-section__round')){
+			button.forEach((element, i) => {
+				if(e.target == element){
+					closeBtn();
+					openBtn(i);
+				}
+			});
+		}
+
+		if(e.target && e.target.classList.contains('wrapper')){
+			closeBtn();
+		}
+	});
+
 })
